b905cbfbd7b390bbf9b4466005f8c80d14a0f4fb
Hwang Hyejung <jimin_ma@naver.com>
2021-12-14 11:47:46 +0900

    etc..

---
+++ lib/components/recipe_list_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem({Key? key, required this.imageName, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+              aspectRatio: 2 / 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.asset(
+                  'assets/images/$imageName.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              )),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 20),
+          ),
+          Text(
+            "Have you ever made your own $title? Once you've tried a homemade $title, you'll never go back.",
+            style: const TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+}
